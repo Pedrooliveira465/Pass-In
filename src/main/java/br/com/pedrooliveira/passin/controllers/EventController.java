4b1dded620c9d52375b438f3305b7d7e3902e424
Pedro Oliveira <pedro86gugalegal@gmail.com>
2024-04-03 08:28:03 -0300

    Improving Eventcontroller

--- src/main/java/br/com/pedrooliveira/passin/controllers/EventController.java
+++ src/main/java/br/com/pedrooliveira/passin/controllers/EventController.java
@@ -1,16 +1,43 @@
 package br.com.pedrooliveira.passin.controllers;
 
+import br.com.pedrooliveira.passin.dto.attendee.AttendeesListResponseDTO;
+import br.com.pedrooliveira.passin.dto.event.EventIdDTO;
+import br.com.pedrooliveira.passin.dto.event.EventRequestDTO;
+import br.com.pedrooliveira.passin.dto.event.EventResponseDTO;
+import br.com.pedrooliveira.passin.services.AttendeeService;
+import br.com.pedrooliveira.passin.services.EventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/events")
+@RequiredArgsConstructor
 public class EventController {
 
-    @GetMapping
-    public ResponseEntity<String> getTeste(){
-        return ResponseEntity.ok("sucesso!");
+    private final EventService eventService;
+    private final AttendeeService attendeeService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EventResponseDTO> getEvent(@PathVariable String id){
+        EventResponseDTO event = this.eventService.getEventDetail(id);
+        return ResponseEntity.ok(event);
+    }
+
+    @PostMapping
+    public ResponseEntity<EventIdDTO> createEvent(@RequestBody EventRequestDTO body, UriComponentsBuilder uriComponentsBuilder){
+        EventIdDTO eventIdDTO = this.eventService.createEvent(body);
+
+        var uri = uriComponentsBuilder.path("/events/{id}").buildAndExpand(eventIdDTO.eventId()).toUri();
+
+        return ResponseEntity.created(uri).body(eventIdDTO);
+    }
+
+    @GetMapping("/attendees/{id}")
+    public ResponseEntity<AttendeesListResponseDTO> getEventAttendee(@PathVariable String id){
+        AttendeesListResponseDTO attendeesListResponse = this.attendeeService.getEventsAttendee(id);
+        return ResponseEntity.ok(attendeesListResponse);
     }
 }
